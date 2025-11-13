199a83eb43bffa0977dc6e326f293fbd1a9e9f86
Vishakh17 <vishakhshetty5@gmail.com>
2025-11-13 15:43:30 +0530

    Refactor dictionary operations and string handling in dic4.py

--- dic4.py
+++ dic4.py
@@ -1,10 +1,10 @@
-# d={'a':10,'b':20,'c':30,'d':40}
-# d.setdefault('f',50)
-# d.update(d=100)
-# print(d.pop("a"))
-# print(d)
+d={'a':10,'b':20,'c':30,'d':40}
+d.setdefault('f',50)
+d.update(d=100)
+print(d.pop("a"))
+print(d)
 
-# print(d.pop('f',50))
+print(d.pop('f',50))
 
 d = {'a':10,'b':20,'c':30,'d':40,'e':50}
 print(d)
@@ -16,6 +16,8 @@ d.items()
 for str1,num in d.items():
     print(str1,":", num)
 
-s = 'Welcome to python'
-s.split()
-print(s)
+s = '  Welcome to python  '
+print(s.strip())
+
+print(s.lstrip())
+
